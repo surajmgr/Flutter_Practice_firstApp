257350e242f4640dbe091ce7f1b1e904457bfa7e
surajmgr <surajpulami8@gmail.com>
2022-07-17 12:58:18 +0545

    ch4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,25 @@ class myApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _nameController = TextEditingController();
+  var myText = "Change Me!";
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text(
           'Demo Home',
@@ -31,7 +44,48 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/bg.jpg",
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width,
+                  //height: MediaQuery.of(context).size.height,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  myText,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter your name.",
+                      labelText: "Name",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -67,7 +121,10 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
         child: const Icon(Icons.edit),
       ),
     );
